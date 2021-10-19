b83296f96cbd225ef4a110c18b530850b76e953c
IglooSyrup <darckuvina@gmail.com>
2021-10-19 11:44:21 -0400

    Backend: Création du projet

---
+++ paimon-quests/src/main/java/com/igloosyrup/paimonquests/PaimonQuestsApplication.java
@@ -0,0 +1,13 @@
+package com.igloosyrup.paimonquests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaimonQuestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaimonQuestsApplication.class, args);
+    }
+
+}
---
+++ paimon-quests/src/test/java/com/igloosyrup/paimonquests/PaimonQuestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igloosyrup.paimonquests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaimonQuestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
