1c9d5e56a9c3fa9180ffcf84fd7ca607b49aa377
IglooSyrup <darckuvina@gmail.com>
2021-10-29 15:54:53 -0400

    Front-end: Ajout des vues

---
+++ paimon-quests/paimon-quests-vue/src/views/PaimonHome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1 class="text-dark">Welcome to Paimon Quests</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'PaimonHome',
+    }
+</script>
+
+<style scoped>
+
+</style>
