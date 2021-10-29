55d4bfd34a46baae9c37c926549fe4e431912797
IglooSyrup <darckuvina@gmail.com>
2021-10-29 15:56:14 -0400

    Front-end: Création du Footer

---
+++ paimon-quests/paimon-quests-vue/src/components/footer/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="text-center bg-primary">
+        <div class="container bg-dark">
+            <span class="text-light display-3">My Footer</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
