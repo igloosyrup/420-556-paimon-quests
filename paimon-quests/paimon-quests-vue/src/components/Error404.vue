e51b6aaad8da228431b7004a4a90a9945370effb
IglooSyrup <darckuvina@gmail.com>
2021-10-29 15:55:31 -0400

    Front-end: Création et Ajout du component Error404

---
+++ paimon-quests/paimon-quests-vue/src/components/Error404.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="text-center mt-5">
+        <h2 class="text-danger">La page n'éxiste pas</h2>
+        <img class="" src="../assets/paimon/no-sleep-paimon.jpg" draggable="false" height="500" alt="">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Erro404'
+    }
+</script>
+
+<style scoped>
+img{
+        -webkit-user-select: none;
+        user-select: none;
+    }
+</style>
