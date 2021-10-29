a8d8e3034f4ae8a88461aa37a1e76f6f2c0181b3
IglooSyrup <darckuvina@gmail.com>
2021-10-29 15:56:39 -0400

    Front-end: Création de la Navbar

---
+++ paimon-quests/paimon-quests-vue/src/components/navbar/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <!--  
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/paimon">Paimon</router-link>
+    Image and text -->
+    <nav class="navbar navbar-light sticky-top bg-light align-middle">
+        <router-link to="/">
+            <a class="navbar-brand">
+                <img class="d-inline-block align-middle flip-img ml-1" draggable="false" :src="require(`@/assets/paimon/${LOGO_SOURCE}`)" width="85" height="85" alt="">
+                <span class="ml-2 align-middle display-4">{{TITLE}}</span>
+            </a>
+        </router-link>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar",
+        created() {
+            this.LOGO_SOURCE = 'hi-paimon.png';
+            this.UN_TEST = "he;lo";
+            this.TITLE = "Paimon Quest"
+        },
+    }
+</script>
+
+<style scoped>
+    .flip-img{
+        -webkit-transform: scaleX(-1);
+        transform: scaleX(-1);
+    }
+
+    img{
+        -webkit-user-select: none;
+        user-select: none;
+    }
+</style>
