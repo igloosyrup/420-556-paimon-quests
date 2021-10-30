e73cc976f3373f0800489e0c099364531e2f2d29
IglooSyrup <darckuvina@gmail.com>
2021-10-30 13:43:14 -0400

    Front-end: Modification du footer + ajout du lien de mon Github

--- paimon-quests/paimon-quests-vue/src/components/footer/Footer.vue
+++ paimon-quests/paimon-quests-vue/src/components/footer/Footer.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="text-center bg-primary">
-        <div class="container bg-dark">
-            <span class="text-light display-3">My Footer</span>
+    <footer class="py-4 charcoal-bg">
+        <div class="text-center mb-4">
+            <h1 class="text-white display-5">Mes contacts</h1>
         </div>
-    </div>
+        <div class="text-center">
+            <a :href="`${GITHUB}`" target="_blank" class="mx-4"><i class="fab fa-github text-light fa-3x"></i></a>
+        </div>
+        <div class="text-center mt-1">
+            <a :href="`${GITHUB}`" target="_blank" class="mx-4 text-light">Github</a>
+        </div>
+        <p class="text-white mt-4 mb-0">&copy; Copyright 2021 - Par Zi Yu Chen </p>
+    </footer>
 </template>
 
 <script>
     export default {
-        name: "Footer"
+        name: "Footer",
+        created(){
+            this.GITHUB = "https://github.com/igloosyrup";
+        }
     }
 </script>
 
