d335ecf5ee9dc210ed7d5ba194950d360f965c70
IglooSyrup <darckuvina@gmail.com>
2021-10-30 17:16:50 -0400

    Front-end: Ajout des vues Login et Register

--- paimon-quests/paimon-quests-vue/src/views/PaimonHome.vue
+++ paimon-quests/paimon-quests-vue/src/views/PaimonHome.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="container">
+    <div class="container mb-3">
         <h1 class="text-dark">Welcome to Paimon Quests</h1>
+        <div>
+            <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+            <b-modal id="modal-1" title="BootstrapVue">
+                <p class="my-4">Hello from modal!</p>
+            </b-modal>
+        </div>
     </div>
 </template>
 
