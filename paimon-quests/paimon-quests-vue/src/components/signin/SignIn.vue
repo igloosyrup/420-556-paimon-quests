a3e21afc4c8ff8c5a1532428af979ceb16ac8d51
IglooSyrup <darckuvina@gmail.com>
2021-11-01 05:24:30 -0400

    Front-end: Ajout des méthodes pour login

--- paimon-quests/paimon-quests-vue/src/components/signin/SignIn.vue
+++ paimon-quests/paimon-quests-vue/src/components/signin/SignIn.vue
@@ -2,42 +2,69 @@
     <div class="container mt-5 pb-5 px-5">
         <div class="py-5 mt-5 mx-5">
             <div class="d-flex justify-content-center">
-                <div class="form-row mt-5">
-                    <h1 class="text-dark mt-5 mx-4 py-5 display-4">
-                        Login
+                <div class="form mt-5">
+                    <img :src="require(`@/assets/paimon/${PAIMON}`)" draggable="false" class="rounded-pill paimon mt-5" alt="">
+                    <h1 class="text-dark mx-4 py-5 display-4">
+                        Se connecter
                     </h1>
                 </div>
             </div>
-            <form class="mb-5">
-                <div class="form-group mb-4">
-                    <div class="d-flex justify-content-center">
-                        <div class="form-group col-md-6">
-                        <label for="inputEmail4">Username</label>
-                        <input type="text" class="form-control" id="inputEmail4" placeholder="Email">
-                        <label for="inputPassword4">Password</label>
-                        <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+            <form class="mb-5" @submit="loginUser">
+                <div class="d-flex justify-content-center">
+                    <div class="form-group">
+                        <div class="mt-3">
+                            <label for="username">Nom d'utilisateur</label>
+                            <input type="text" v-model="userName" class="form-control" id="username" placeholder="Votre nom d'utilisateur">
+                        </div>
+                        <div class="mt-3">
+                            <label for="password">Mot de passe</label>
+                            <input type="password" v-model="password" class="form-control" id="password" placeholder="Votre mot de passe">
                         </div>
                     </div>
                 </div>
-                <button type="submit" class="btn btn-primary mt-5">Sign in</button>
+                <button type="submit" class="btn btn-primary mt-5">Se connecter</button>
             </form>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
     export default {
         name: "SignIn",
+        data(){
+            return{
+                userName: "",
+                password: ""
+            }
+        },
         created(){
-            this.PAIMON = 'nice-paimon.png'
+            this.PAIMON = 'Paimon-Treasure.png'
+        },
+        methods:{
+            clearForm(){
+                this.userName = ''
+                this.password = ''
+            },
+            loginSuccess(){
+                this.clearForm()
+                alert("Connexion avec succès!")
+            },
+            loginUser(e){
+                e.preventDefault();
+                axios.get(`http://localhost:9696/user/login/${this.userName}/${this.password}`)
+                    .then(res => res.data.userName == this.userName ? this.loginSuccess() : alert("Connexion échouée"))
+            }
         }
     }
 </script>
 
 <style scoped>
     input{
-            background-color: rgba(255,255,255, 0.0);
+            /* background-color: rgba(255,255,255, 0.0); */
             border: 0px;
     }
-
+    .paimon{
+        width: 197.5px;
+    }
 </style>
