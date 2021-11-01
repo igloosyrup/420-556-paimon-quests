9775505c1d99946f184e589c3b2037323c28c4db
IglooSyrup <darckuvina@gmail.com>
2021-10-31 23:59:19 -0400

    Back-end: Ajout des methodes Login et Registration

--- paimon-quests/src/main/java/com/igloosyrup/paimonquests/controllers/UserController.java
+++ paimon-quests/src/main/java/com/igloosyrup/paimonquests/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.igloosyrup.paimonquests.controllers;
 
+import com.igloosyrup.paimonquests.models.User;
 import com.igloosyrup.paimonquests.services.UserService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4000")
+@RequestMapping("/user")
 public class UserController {
 
     public UserService userService;
@@ -14,6 +17,17 @@ public class UserController {
         this.userService = userService;
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User newUser){
+        return userService.registerUser(newUser)
+                .map(user1 -> ResponseEntity.status(HttpStatus.CREATED).body(user1))
+                .orElse(ResponseEntity.status(HttpStatus.CONFLICT).build());
+    }
 
-
+    @GetMapping("/login/{userName}/{password}")
+    public ResponseEntity<User> loginUser(@PathVariable String userName, @PathVariable String password){
+        return userService.loginUser(userName, password)
+                .map(user1 -> ResponseEntity.status(HttpStatus.OK).body(user1))
+                .orElse(ResponseEntity.status(HttpStatus.CONFLICT).build());
+    }
 }
