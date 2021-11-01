6b255c10fc8f62e881bf52ed134e736a25d88a07
IglooSyrup <darckuvina@gmail.com>
2021-11-01 00:01:05 -0400

    Back-end: Ajout des methodes Login et Registration

--- paimon-quests/src/main/java/com/igloosyrup/paimonquests/services/UserService.java
+++ paimon-quests/src/main/java/com/igloosyrup/paimonquests/services/UserService.java
@@ -1,14 +1,33 @@
 package com.igloosyrup.paimonquests.services;
 
+import com.igloosyrup.paimonquests.models.User;
 import com.igloosyrup.paimonquests.repositories.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     public UserRepository userRepository;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public Optional<User> registerUser(User user) {
+        try {
+            return Optional.of(userRepository.save(user));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<User> loginUser(String userName, String password) {
+        try {
+            return Optional.of(userRepository.findUserByUserNameAndPassword(userName, password));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
     }
 }
