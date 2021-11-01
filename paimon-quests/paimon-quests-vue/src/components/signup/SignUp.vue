0cb89cecf9d62d7f6664e75d0f1e0b6b51bb1d6a
IglooSyrup <darckuvina@gmail.com>
2021-11-01 05:24:54 -0400

    Front-end: Ajout des méthodes pour register

--- paimon-quests/paimon-quests-vue/src/components/signup/SignUp.vue
+++ paimon-quests/paimon-quests-vue/src/components/signup/SignUp.vue
@@ -1,52 +1,74 @@
 <template>
-    <div class="container mt-5 pb-5 px-5">
-        <div class="py-5 mt-5 mx-5">
-            <div class="d-flex justify-content-center">
-                <div class="form-row mt-5">
-                    <img :src="require(`@/assets/paimon/${PAIMON}`)" draggable="false" class="rounded-pill ml-3 paimon " alt="">
-                    <h1 class="text-dark mt-5 mx-4 py-5 display-4">
-                        Inscrivez-vous
-                    </h1>
-                    <img :src="require(`@/assets/paimon/${PAIMON}`)" draggable="false" class="rounded-pill mx-3 flip-img paimon" alt="">
-                </div>
-            </div>
-            <form class="my-5">
-                <div class="form-row mt-4">
-                    <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    <div class="container mt-5 pb-5 px-5 mr-5">
+        <div class="py-5 mt-5">
+                <img :src="require(`@/assets/paimon/${PAIMON}`)" draggable="false" class="rounded-pill paimon mt-5" alt="">
+                <h1 class="text-dark py-5 display-4">
+                    Inscrivez-vous
+                </h1>
+                <form class="my-5 mx-5" @submit="registerUser">
+                    <div class="d-flex justify-content-center mt-2">
+                        <div class="form-group">
+                            <label for="username">Nom d'utilisateur</label>
+                            <input type="text" class="form-control" id="username" v-model="userName" placeholder="Votre nom d'utilisateur">
+                        </div>
                     </div>
-                    <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+                    <div class="d-flex justify-content-center mt-2">
+                        <div class="form-group">
+                            <label for="email">Adresse Courriel</label>
+                            <input type="email" class="form-control" id="email" v-model="email" placeholder="Votre adresse courriel">
+                        </div>
                     </div>
-                </div>
-                <div class="form-group mt-4">
-                    <label for="inputAddress">Address</label>
-                    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-                </div>
-                <div class="form-group mt-4">
-                    <label for="inputAddress2">Address 2</label>
-                    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                </div>
-                <button type="submit" class="btn btn-primary">Sign in</button>
-            </form>
+                    <div class="d-flex justify-content-center mt-2">
+                        <div class="form-group">
+                            <label for="password">Mot de passe</label>
+                            <input type="password" class="form-control" id="password" v-model="password" placeholder="Votre mot de passe">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-5">S'inscrire</button>
+                </form>
         </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
     export default {
         name: "SignUp",
+        data(){
+            return{
+                userName: "",
+                password: "",
+                email: ""
+            }
+        },
         created(){
             this.PAIMON = 'nice-paimon.png'
+        },
+        methods:{
+            clearForm(){
+                this.userName = ''
+                this.password = ''
+                this.email = ''
+            },
+            registerSuccess(asd){
+                console.log(asd)
+                this.clearForm()
+                alert("Inscription avec succès!")
+            },
+            registerUser(e){
+                e.preventDefault();
+                const user = { userName : this.userName, password : this.password, email : this.email}
+                axios.post('http://localhost:9696/user/register', user)
+                    .then(res => res.data.password == this.password ? this.registerSuccess(res) : alert("Insciption échouée"))
+                    .catch(err => console.log(err))
+            }
         }
     }
 </script>
 
 <style scoped>
     input{
-        background-color: rgba(255,255,255, 0.0);
+        /* background-color: rgba(255,255,255, 0.0); */
         border: 0px;
     }
     .paimon{
