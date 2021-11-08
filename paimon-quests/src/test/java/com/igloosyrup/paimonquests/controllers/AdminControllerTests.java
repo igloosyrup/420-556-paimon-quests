d4f8c7fc3e1b9574e4cac062d5dcc692f63f1681
IglooSyrup <darckuvina@gmail.com>
2021-11-08 04:36:35 -0500

    Backend: Création des classes tests pour Admin

--- paimon-quests/src/test/java/com/igloosyrup/paimonquests/controllers/AdminControllerTests.java
+++ paimon-quests/src/test/java/com/igloosyrup/paimonquests/controllers/AdminControllerTests.java
@@ -1,2 +1,34 @@
-package com.igloosyrup.paimonquests.controllers;public class AdminControllerTests {
+package com.igloosyrup.paimonquests.controllers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.igloosyrup.paimonquests.services.AdminService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+@WebMvcTest(UserController.class)
+public class AdminControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AdminService adminService;
+
+
 }
--- paimon-quests/src/test/java/com/igloosyrup/paimonquests/services/AdminServiceTests.java
+++ paimon-quests/src/test/java/com/igloosyrup/paimonquests/services/AdminServiceTests.java
@@ -1,2 +1,15 @@
-package com.igloosyrup.paimonquests.services;public class AdminServiceTests {
+package com.igloosyrup.paimonquests.services;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+public class AdminServiceTests {
 }
