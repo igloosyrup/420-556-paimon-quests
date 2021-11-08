11c3d04940820e3ec259935445d29c2e808c1ef0
IglooSyrup <darckuvina@gmail.com>
2021-11-08 04:54:37 -0500

    Backend: Ajout du model Admin

--- paimon-quests/src/main/java/com/igloosyrup/paimonquests/models/Admin.java
+++ paimon-quests/src/main/java/com/igloosyrup/paimonquests/models/Admin.java
@@ -1,2 +1,24 @@
-package com.igloosyrup.paimonquests.models;public class Admin {
+package com.igloosyrup.paimonquests.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Admin {
+    @Transient
+    private final int USERNAME_LENGTH = 20;
+    @Transient
+    private final int EMAIL_LENGTH = 50;
+    @Id
+    @GeneratedValue
+    private Integer idUser;
+    @Column(unique = true, length = USERNAME_LENGTH)
+    private String userName;
+    private String password;
+    @Column(unique = true, length = EMAIL_LENGTH)
+    private String email;
+    private String firstName;
+    private String lastName;
 }
