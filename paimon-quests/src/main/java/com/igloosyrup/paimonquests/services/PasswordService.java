072ab35422b54df6b740cfa0063f760c0720646f
IglooSyrup <darckuvina@gmail.com>
2021-11-08 04:59:26 -0500

    Backend: Ajout du Service PasswordService pour l'encodage de mot de passe

--- paimon-quests/src/main/java/com/igloosyrup/paimonquests/services/PasswordService.java
+++ paimon-quests/src/main/java/com/igloosyrup/paimonquests/services/PasswordService.java
@@ -1,2 +1,36 @@
-package com.igloosyrup.paimonquests.services;public class PasswordService {
+package com.igloosyrup.paimonquests.services;
+
+import com.igloosyrup.paimonquests.enums.PasswordEnum;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class PasswordService {
+
+    private Map<String, PasswordEncoder> encoders;
+
+    private PasswordEncoder passwordEncoder;
+
+    public PasswordService(String idEncode) {
+        encoders = new HashMap<>();
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put(PasswordEnum.PBKDF2.getStringValue(), new Pbkdf2PasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder());
+
+        passwordEncoder = new DelegatingPasswordEncoder(idEncode, encoders);
+    }
+
+    public String encodePassword(String password){
+        return passwordEncoder.encode(password);
+    }
+
+    public boolean matchPassword(String password, String encoded){
+        return passwordEncoder.matches(password, encoded);
+    }
 }
