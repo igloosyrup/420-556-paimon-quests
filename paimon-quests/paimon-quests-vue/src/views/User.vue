fbbaac502776a5fc702ef8f74cfb488a06e1f344
IglooSyrup <darckuvina@gmail.com>
2021-11-08 04:51:21 -0500

    Frontend: Création de la vue Admin et User

---
+++ paimon-quests/paimon-quests-vue/src/views/User.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container-fluid py-5 paimon-bg">
+        <UserProfile />
+    </div>
+</template>
+
+<script>
+import UserProfile from '@/components/profile/UserProfile'
+    export default {
+        name: "User",
+        components:{
+            UserProfile,
+        }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
