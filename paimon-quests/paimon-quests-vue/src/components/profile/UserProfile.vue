b245bb198b76ec4cd1757bc7348aceb83806a46b
IglooSyrup <darckuvina@gmail.com>
2021-11-08 04:51:56 -0500

    Frontend: Ajout des components Profile

---
+++ paimon-quests/paimon-quests-vue/src/components/profile/UserProfile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-light">Mon Profil</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "UserProfile",
+
+    }
+</script>
+
+<style scoped>
+
+</style>
