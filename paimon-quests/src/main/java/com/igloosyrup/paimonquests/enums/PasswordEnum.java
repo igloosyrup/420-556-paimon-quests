5f35fd34fee415fb9e1299f4323e77d69f711a05
IglooSyrup <darckuvina@gmail.com>
2021-11-08 04:56:05 -0500

    Backend: Ajout d'un enum pour password encoder

--- paimon-quests/src/main/java/com/igloosyrup/paimonquests/enums/PasswordEnum.java
+++ paimon-quests/src/main/java/com/igloosyrup/paimonquests/enums/PasswordEnum.java
@@ -1,2 +1,17 @@
-package com.igloosyrup.paimonquests.enums;public enum PasswordEnum {
+package com.igloosyrup.paimonquests.enums;
+
+public enum PasswordEnum {
+
+
+    PBKDF2("pbkdf2");
+
+    PasswordEnum(String stringValue){
+        this.stringValue = stringValue;
+    }
+
+    private final String stringValue;
+
+    public String getStringValue(){
+        return stringValue;
+    }
 }
