125fe6b0e406a6d70785b80d04a3384c41e87f8f
IglooSyrup <darckuvina@gmail.com>
2021-11-15 04:13:07 -0500

    Back-end: Desactiver la protection contre les attaques CSRF

--- paimon-quests/src/main/java/com/igloosyrup/paimonquests/configuration/WebSecurityConfigurer.java
+++ paimon-quests/src/main/java/com/igloosyrup/paimonquests/configuration/WebSecurityConfigurer.java
@@ -3,6 +3,7 @@ package com.igloosyrup.paimonquests.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -10,5 +11,8 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
+//        http.csrf().ignoringAntMatchers("/user/login/**");
+//        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+//        http.formLogin().successHandler()
     }
 }
