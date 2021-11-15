179dec82cc3cbe84cc7cace617eae0b78d159faf
IglooSyrup <darckuvina@gmail.com>
2021-11-15 04:10:24 -0500

    Front-end: Ajout de la vue EditUser

---
+++ paimon-quests/paimon-quests-vue/src/views/EditUser.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container-fluid py-5 paimon-bg">
+        <div class="container py-5 my-5">
+            <div class="form-bg py-5">
+                <EditUserProfile />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import EditUserProfile from '@/components/edit-profile/EditUserProfile'
+    export default {
+        name: "EditUser",
+        components: {
+            EditUserProfile,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
