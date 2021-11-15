2ec847a2793fbfeff4299b7ef23c77e0f18791c1
IglooSyrup <darckuvina@gmail.com>
2021-11-15 04:09:13 -0500

    Front-end: Ajout + Création du component Welcome

---
+++ paimon-quests/paimon-quests-vue/src/components/home/Welcome.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container mb-3">
+        <div>
+            <img :src="require(`@/assets/paimon/${PAIMON}`)" draggable="false" class="paimon mt-5">
+
+
+            <!-- <b-button v-b-modal.modal-1>Launch demo modal</b-button>
+
+            <b-modal id="modal-1" title="BootstrapVue">
+                <p class="my-4">Hello from modal!</p>
+            </b-modal> -->
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Welcome',
+        created(){
+            this.PAIMON = 'qiqi.png'
+        },
+    }
+</script>
+
+<style scoped>
+    .paimon{
+        width: 200px
+    }
+</style>
