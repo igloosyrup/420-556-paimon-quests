c108c1007935dcde97744f0d4ac754f0f70cb80a
IglooSyrup <darckuvina@gmail.com>
2021-11-15 04:08:00 -0500

    Front-end: Création de la vue UserProfile

--- paimon-quests/paimon-quests-vue/src/components/profile/UserProfile.vue
+++ paimon-quests/paimon-quests-vue/src/components/profile/UserProfile.vue
@@ -1,13 +1,39 @@
 <template>
-    <div>
-        <h1 class="text-light">Mon Profil</h1>
+    <div class="mt-5 py-5 text-light">
+        <h1>Profil de {{this.$store.getters.currentUser.userName}}</h1>
+        <div class="container-fluid d-flex justify-content-between mt-5">
+            <div class="col-sm-12 col-md-6">
+                <h2 class="mt-2">
+                    Mes informations personnelles
+                </h2>
+                <div class="mt-4">
+                    <router-link to="/edit-user">
+                        <a class="btn btn-success p-2 mx-2 mt-5">
+                            Modifier mon profil
+                        </a>
+                    </router-link>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <h2 class="mt-2">
+                    Mes statistiques
+                </h2>
+                <div class="mt-4">
+                    <router-link to="/new-quest">
+                        <a class="btn btn-success p-2 mx-2 mt-5">
+                            Ajouter une nouvelle quête
+                        </a>
+                    </router-link>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: "UserProfile",
-
     }
 </script>
 
