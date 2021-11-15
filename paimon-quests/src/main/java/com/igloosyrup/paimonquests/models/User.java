f58b12799dd1ae95236b8fb40f7839042af9e218
IglooSyrup <darckuvina@gmail.com>
2021-11-15 04:15:10 -0500

    Back-end: Enlever les champs inutiles

--- paimon-quests/src/main/java/com/igloosyrup/paimonquests/models/User.java
+++ paimon-quests/src/main/java/com/igloosyrup/paimonquests/models/User.java
@@ -1,6 +1,5 @@
 package com.igloosyrup.paimonquests.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,27 +7,23 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.time.LocalDate;
 
 @Data
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class User implements Serializable {
 
-    @Transient
-    private final int USERNAME_LENGTH = 20;
-    @Transient
-    private final int EMAIL_LENGTH = 50;
     @Id
     @GeneratedValue
     private Integer idUser;
-    @Column(unique = true, length = USERNAME_LENGTH)
+
+    @Column(unique = true, length = 20)
     private String userName;
+
     private String password;
-    @Column(unique = true, length = EMAIL_LENGTH)
+    @Column(unique = true, length = 50)
     private String email;
     private String firstName;
     private String lastName;
