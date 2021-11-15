50836da390ba000b2732f40cca72f2c07b9ccdec
IglooSyrup <darckuvina@gmail.com>
2021-11-15 04:11:41 -0500

    Front-end: Ajout des import manquantes

--- paimon-quests/paimon-quests-vue/src/main.js
+++ paimon-quests/paimon-quests-vue/src/main.js
@@ -7,15 +7,22 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@/assets/css/styles.css'
+import VueCookies from 'vue-cookies'
 import Vuelidate from 'vuelidate'
+import store from '@/store/store.js'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
+Vue.use(VueSweetalert2)
 Vue.config.productionTip = false
 Vue.use(Vuelidate)
+Vue.use(VueCookies)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(Vuex)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
